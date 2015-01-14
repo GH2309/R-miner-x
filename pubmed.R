cd2ca9b590e0eb60767e77f43e88456215b4c467
miner-x <jason.a.flint@gmail.com>
2015-01-14 17:02:16 -0500

    removed extra code not being used

--- pubmed.R
+++ pubmed.R
@@ -19,10 +19,4 @@ if (count>100)stop("Too many cases. Try to refine search terms")else
  ## Download all abstracts
 look <- paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=",db,"&id=",uidlist,"&rettype=abstract&retmode=text")
 suppressWarnings(download.file(url=look,destfile=paste0(search.term,".txt")))
-# ##download a summary for each uid
-#  look2 <- paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=",db,"&id=",uidlist,"&version=2.0")
-#  filename <- paste0(search.term,".xml")
-#  download.file(url=look2,destfile=filename)
-#  summary <- xmlSApply(filename,xmlToDataFrame)
-# head(summary)
-}
+
