dce3bb91987fe6c0217652b1e3cda40943684c09
Jason Flint <dataminer-x@users.noreply.github.com>
2016-01-06 09:59:00 -0500

    Create BContin.R
    This is an example of how I think about Bayes as it relates to biomedical tests. This function starts from the number of true and false positives and negatives, and calculates the PPV, NPV, Sn, SP, and Diagnostic Odds Ratio.
    
    There could certainly be an adaptation of this function to take Sn, Sp, and Prevalence, and calculate everything else, I just haven't written it yet.

---
+++ BContin.R
@@ -0,0 +1,22 @@
+contin <- function (a,b,c,d) {
+# a= True Positive
+# b= False Positive
+# c= False Negative
+# d= True Negative
+# This function assumes that the test result is on the left with the Positive result on top, and the Condition is on the Top, with the positive condition on the left.
+#            D+  | D-
+#        T+  a   |  b   |      
+#        ----------------
+#       T-   c   |  d   | 
+       
+  sensitivity <- (a/(a+c))
+  specificity <- (d/(b+d))
+  prevalence <- ((a+c)/(a+b+c+d))
+  PPV <- (sensitivity*prevalence)/((sensitivity*prevalence)+((1-specificity)*(1-prevalence)))
+  NPV <- ((d)/(c+d))
+  DOR <- ((a/c)/(b/d))
+  names <- c("Sensitivity = ","Specificity = ","Prevalence = ", "PPV = ", "NPV = ", "Diagnostic Odds Ratio = ")
+  results <- c(sensitivity,specificity,prevalence,PPV,NPV,DOR)
+  table <- data.frame(names,results)
+return(table)
+}
